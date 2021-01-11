c4742af1956885ffd7b9c6cd39998306551aeaba
Rupam <hrupam55@gmail.com>
2021-01-11 14:33:41 +0530

    initial commit

--- case/views.py
+++ case/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.list import ListView
+from .models import Case
+
 
 # Create your views here.
+class CaseListView(ListView):
+    paginate_by = 30
+
+    def get_queryset(self):
+        queryset=Case.objects.all()
+        if self.kwargs.get('category'):
+            queryset=queryset.filter(category=self.kwargs.get('category'))
+        return queryset
+
+# class CaseView(View):
