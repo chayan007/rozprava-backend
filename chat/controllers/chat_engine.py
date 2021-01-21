cfd5365f49a7cad65e493cb17e58324fc21078bd
chayan007 <sonicxxx7@gmail.com>
2021-01-21 15:37:47 +0530

    chat engine list messages

--- chat/controllers/chat_engine.py
+++ chat/controllers/chat_engine.py
@@ -5,9 +5,9 @@ from profiles.models import Profile
 
 class ChatEngine:
 
-    def __init__(self, sender_profile_uuid, receiver_profile_uuid):
-        self.sender = Profile.objects.get(uuid=sender_profile_uuid)
-        self.receiver = Profile.objects.get(uuid=receiver_profile_uuid)
+    def __init__(self, sender_profile_uuid=None, receiver_profile_uuid=None):
+        self.sender = Profile.objects.get(uuid=sender_profile_uuid) if sender_profile_uuid else None
+        self.receiver = Profile.objects.get(uuid=receiver_profile_uuid) if sender_profile_uuid else None
 
     def send(self, **kwargs):
         """Send message handler."""
@@ -25,9 +25,24 @@ class ChatEngine:
     def receive(self, **kwargs):
         """Send message handler."""
         try:
-            return OneToOneMessage.objects.filter(
+            messages = OneToOneMessage.objects.filter(
                 sender=self.sender,
                 receiver=self.receiver
             )
+            self.update_message_as_seen(messages)
+            return messages
         except BaseException:
             return False
+
+    @staticmethod
+    def update_message_as_seen(messages: [OneToOneMessage]):
+        for message in messages:
+            if not message.is_seen:
+                message.is_seen = True
+                message.save()
+
+    def list_messages(self):
+        try:
+            return OneToOneMessage.objects.filter(receiver=self.receiver)
+        except BaseException:
+            return
