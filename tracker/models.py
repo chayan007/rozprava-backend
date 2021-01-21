2f98f370a71b60d394c3bc4feb258ea5863c8a49
chayan007 <sonicxxx7@gmail.com>
2021-01-21 15:42:32 +0530

    Tracker model base created

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from base.models import BaseModel
+
+from profiles.models import Profile
+
+
+class LocationTracker(BaseModel):
+    """Store all user location."""
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    ip_address = models.GenericIPAddressField()
+    location = models.JSONField()
+
+    def __str__(self):
+        return f'{self.profile.user.username}: {self.ip_address}'
