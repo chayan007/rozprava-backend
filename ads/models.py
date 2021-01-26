cacc2f71d9e9ab5eb28c1f05dc3eef14052014c5
chayan007 <sonicxxx7@gmail.com>
2021-01-27 00:30:29 +0530

    Ad model created

--- ads/models.py
+++ ads/models.py
@@ -19,6 +19,7 @@ class Ad(BaseModel):
     starting_date = models.DateField()
     ending_date = models.DateField()
     payment_id = models.CharField(max_length=200, null=True, blank=True)
+    is_event = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
