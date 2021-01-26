3f0fbbacdb80a00ce1dbe0f2e7b339c938435a4d
chayan007 <sonicxxx7@gmail.com>
2021-01-26 10:35:40 +0530

    aligned rebuttals with debate

--- debate/controllers/debate_handler.py
+++ debate/controllers/debate_handler.py
@@ -16,27 +16,35 @@ class DebateHandler:
         self.case = Case.objects.get(uuid=case_uuid)
 
     @staticmethod
-    def get_based_on_case(case_uuid: str):
+    def get_based_on_case(case_uuid: str) -> [Debate]:
         case = Case.objects.get(uuid=case_uuid)
-        debates = list(
-            Debate.objects.filter(
-                case=case,
-                pointer__isnull=True
-            )
+        debates = Debate.objects.filter(
+            case=case,
+            pointer__isnull=True
         )
-        rebuttal_debates = Debate.objects.filter(
-            pointer__in=debates
-        ).order_by('pointer', 'created_at')
-        rebuttal_debate_pointer_uuids = [debate.uuid for debate in rebuttal_debates]
-        sorted_debate_list = []
-        for rebuttal in rebuttal_debates:
-            pass
+        return debates
 
     @staticmethod
-    def get(debate_uuid: str):
+    def get_rebuttals_for_debate(debate: Debate) -> [Debate]:
+        return Debate.objects.filter(
+            pointer=debate
+        ).order_by('created_at')
+
+    def align_rebuttals_with_debates(self, debates: [Debate]) -> [Debate]:
+        aligned_debates = []
+        for debate in debates:
+            aligned_debates.append(debate)
+            rebuttals = self.get_rebuttals_for_debate(debate)
+            if not rebuttals:
+                continue
+            aligned_debates.extend(rebuttals)
+        return aligned_debates
+
+    @staticmethod
+    def get(debate_uuid: str) -> Debate:
         return get_object_or_404(Debate, uuid=debate_uuid)
 
-    def create(self, user, ip_address, **kwargs):
+    def create(self, user, ip_address, **kwargs) -> Debate:
         debate = Debate.objects.create(**{
             'profile': user.profile,
             'case': self.case,
@@ -46,7 +54,7 @@ class DebateHandler:
         })
         return debate
 
-    def create_rebuttal(self, user, **kwargs):
+    def create_rebuttal(self, user, **kwargs) -> Debate:
         original_debate = self.get(kwargs['debate_uuid'])
         if original_debate and not original_debate.pointer:
             debate = Debate.objects.create(**{
@@ -60,7 +68,7 @@ class DebateHandler:
         raise RebuttalFailedException('Failed to upload rebuttal to the specific debate.')
 
     @staticmethod
-    def update(debate_uuid, **kwargs):
+    def update(debate_uuid, **kwargs) -> Debate:
         debate = Debate.objects.get(uuid=debate_uuid)
         if kwargs.get('comment') and (get_string_matching_coefficient(debate.comment, kwargs.get('comment'))) > 0.8:
             debate.comment = kwargs.get('comment')
@@ -69,6 +77,6 @@ class DebateHandler:
         return debate
 
     @staticmethod
-    def delete(debate_uuid):
+    def delete(debate_uuid) -> None:
         debate = Debate.objects.get(uuid=debate_uuid)
         debate.delete()
