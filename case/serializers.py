95070749419ebf9f187e829337d5e54b968fefe3
chayan007 <sonicxxx7@gmail.com>
2021-02-07 17:26:39 +0530

    CaseSerializer (1/2)

--- case/serializers.py
+++ case/serializers.py
@@ -1,2 +1,22 @@
-class CaseSerializer:
+from rest_framework import serializers
+
+from case.models import Case
+
+
+class CaseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Case
+        fields = (
+            'profile',
+            'question',
+            'description',
+            'category',
+            'slug',
+            'for_label',
+            'against_label'
+        )
+
+
+class CaseDetailedSerializer:
     pass
