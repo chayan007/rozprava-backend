6736e543f945825eb92cb259fa1901a5bf51ec67
chayan007 <sonicxxx7@gmail.com>
2021-02-22 15:33:46 +0530

    added post() activity case status

--- case/views/v1.py
+++ case/views/v1.py
@@ -1,7 +1,13 @@
+from rest_framework import status
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from activity.models import Activity
 from case.models import Case
 from case.serializers import CaseSerializer
+
+from profiles.exceptions import UserValidationFailedException
 
 
 class CaseListView(ListAPIView):
@@ -26,3 +32,27 @@ class CaseDetailView(RetrieveAPIView):
     lookup_field = 'slug'
     queryset = Case.objects.all()
     serializer_class = CaseSerializer
+
+
+class CaseActivityView(APIView):
+    """Record any activity on case as Like or Report."""
+
+    def post(self, request, *args, **kwargs):
+        profile = request.user.profile
+        if not profile:
+            raise UserValidationFailedException('You need to be logged in to perform this action!')
+
+        case_uuid = kwargs.get('case_uuid')
+        activity_type = kwargs.get('activity_type')
+
+        if case_uuid and activity_type:
+            case = Case.objects.get(uuid=case_uuid)
+            case.activities.create(activity_type=activity_type, profile=profile)
+            return Response(
+                data={'message': 'Wow! You have interacted with the post.'},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data={'message': 'Oops! Something went wrong. Try again later.'},
+            status=status.HTTP_304_NOT_MODIFIED
+        )
