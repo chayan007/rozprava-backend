fb08af20c6e8aa10e7e39525095e6b4cda6e4294
chayan007 <sonicxxx7@gmail.com>
2021-02-23 14:34:58 +0530

    get_debate_metrics() added

--- debate/utilities.py
+++ debate/utilities.py
@@ -1,8 +1,13 @@
+from activity.models import Activity
+
 from debate.models import Debate
 
 
 def get_debate_metrics(debate: Debate):
     """Get debate activity metrics."""
     return [
-        1
+        Activity.objects.filter(content_type=Debate, object_id=debate.id, activity_type=Activity.ActivityChoices.REPORT),
+        Activity.objects.filter(content_type=Debate, object_id=debate.id, activity_type=Activity.ActivityChoices.UPVOTE),
+        Activity.objects.filter(content_type=Debate, object_id=debate.id, activity_type=Activity.ActivityChoices.DOWNVOTE),
+        Activity.objects.filter(content_type=Debate, object_id=debate.id, activity_type=Activity.ActivityChoices.VIEW)
     ]
