c5957a49194b124701a8aec4a7912a71260ddcea
chayan007 <sonicxxx7@gmail.com>
2021-03-09 21:44:51 +0530

    ChatView implemented - boilerplate

---
+++ chat/views/v1.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+
+
+class ChatView(APIView):
+    """Chat view for users."""
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
