464ea7aa8ddb9528b7de346b8f703693860d8573
chayan007 <sonicxxx7@gmail.com>
2021-03-10 13:19:34 +0530

    changed business logic for chatting

--- chat/controllers/chat_engine.py
+++ chat/controllers/chat_engine.py
@@ -1,3 +1,5 @@
+from django.db.models import Count, Q
+
 from chat.models import OneToOneMessage
 
 from profiles.models import Profile
@@ -5,7 +7,7 @@ from profiles.models import Profile
 
 class ChatEngine:
 
-    def __init__(self, sender_profile_uuid=None, receiver_profile_uuid=None):
+    def __init__(self,  receiver_profile_uuid, sender_profile_uuid=None):
         self.sender = Profile.objects.get(uuid=sender_profile_uuid) if sender_profile_uuid else None
         self.receiver = Profile.objects.get(uuid=receiver_profile_uuid) if sender_profile_uuid else None
 
@@ -23,7 +25,7 @@ class ChatEngine:
             return False
 
     def receive(self, **kwargs):
-        """Send message handler."""
+        """Receive message handler."""
         try:
             messages = OneToOneMessage.objects.filter(
                 sender=self.sender,
@@ -33,6 +35,14 @@ class ChatEngine:
             return messages
         except BaseException:
             return False
+
+    def show_messaging_list(self):
+        """Show messages unread/read grouped by user."""
+        return OneToOneMessage.objects.filter(
+            receiver=self.receiver
+        ).aggregate(
+            no_of_unreads=Count('is_seen', filter=Q())
+        ).distinct('profile').order_by('created_at')
 
     @staticmethod
     def update_message_as_seen(messages: [OneToOneMessage]):
