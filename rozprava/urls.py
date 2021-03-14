8c179a56b828ef4f7b51aa830ffe04629547446c
chayan007 <sonicxxx7@gmail.com>
2021-03-14 23:46:19 +0530

    urls.py revamp and modular

--- rozprava/urls.py
+++ rozprava/urls.py
@@ -1,12 +1,55 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
+"""
+Core Rozprava Business Logic Routes
+"""
+
+core_urls = [
     path('admin/', admin.site.urls),
     path('profile/', include('profiles.urls')),
 ]
+
+"""
+DRF Swagger Documentation Routes
+"""
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Rozprava API",
+      default_version='v1',
+      description="The ultimate platform for debaters.",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+swagger_urls = [
+   path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   ...
+]
+
+
+"""
+Compile different Routes into singular list.
+"""
+
+urlpatterns = core_urls + swagger_urls
+
+
+"""
+Implement static and media routes for development purposes.
+"""
 
 if settings.DEBUG:
     urlpatterns += static(
