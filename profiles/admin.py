5be9b0f43d040f0a872445d0e7ba6b95c96009d7
chayan007 <sonicxxx7@gmail.com>
2021-04-22 22:17:27 +0530

    @ -> normal func

--- profiles/admin.py
+++ profiles/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from profiles.models import FollowerMap, IdentityDocument, Group, Profile
 
 
-@admin.site.register(Profile)
-@admin.site.register(FollowerMap)
-@admin.site.register(IdentityDocument)
-@admin.site.register(Group)
+admin.site.register(Profile)
+admin.site.register(FollowerMap)
+admin.site.register(IdentityDocument)
+admin.site.register(Group)
