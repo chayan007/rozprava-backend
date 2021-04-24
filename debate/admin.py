ac7bf640c5648db2ca676b6f1096c7460a2266d1
chayan007 <sonicxxx7@gmail.com>
2021-04-25 01:38:07 +0530

    admin for debate models

--- debate/admin.py
+++ debate/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from debate.models import Debate, DebateImpactHit
+
+admin.site.register(Debate)
+admin.site.register(DebateImpactHit)
