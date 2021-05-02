6698ca0f8e515673dbed21498dad27b4689a1da5
chayan007 <sonicxxx7@gmail.com>
2021-05-02 17:49:20 +0530

    added impact handler functions

--- debate/controllers/debate_impact_handler.py
+++ debate/controllers/debate_impact_handler.py
@@ -1,10 +1,25 @@
+from django.db import IntegrityError
+
+from debate.models import Debate, DebateImpactHit
+
+from profiles.models import Profile
+
+
 class DebateImpactHandler:
 
-    def __init__(self):
-        pass
+    def __init__(self, debate_uuid: str):
+        self.debate = Debate.objects.get(uuid=debate_uuid)
 
-    def rate(self):
-        pass
+    def rate(self, profile: Profile, rating: int):
+        try:
+            impact_obj = DebateImpactHit.objects.create(
+                profile=profile,
+                impact=rating,
+                debate=self.debate
+            )
+            return impact_obj
+        except (IntegrityError, ValueError):
+            return False
 
-    def get(self):
-        pass
+    def get_impact(self):
+        return DebateImpactHit.objects.get(debate=self.debate)
