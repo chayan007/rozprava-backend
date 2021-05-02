86c4d2741902415bec246784ffe189f7132d691e
chayan007 <sonicxxx7@gmail.com>
2021-05-02 18:33:37 +0530

    Boost model added

--- ads/models.py
+++ ads/models.py
@@ -1,8 +1,15 @@
+import datetime
+
 from django.db import models
 
-from ads.utilities import get_ads_image_upload_path, get_ads_video_upload_path
+from ads.utilities import (
+    get_ads_image_upload_path,
+    get_ads_video_upload_path
+)
 
 from base.models import BaseModel
+
+from case.models import Case
 
 from profiles.models import Profile
 
@@ -23,3 +30,16 @@ class Ad(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Boost(BaseModel):
+    """Store case boost request."""
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    case = models.ForeignKey(Case, on_delete=models.CASCADE)
+    amount = models.DecimalField()
+    start_date = models.DateField(default=datetime.date.today())
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.profile.user.get_full_name()}: {self.amount}'
