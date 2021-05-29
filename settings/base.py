7b6f2d4359c8c8ff0dc7fee174ef788f9e41261d
chayan007 <sonicxxx7@gmail.com>
2021-05-30 01:05:19 +0530

    adds setting changes

--- settings/base.py
+++ settings/base.py
@@ -107,10 +107,15 @@ WSGI_APPLICATION = 'rozprava.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('POSTGRES_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('POSTGRES_DB', BASE_DIR / 'db.sqlite3'),
+        'USER': os.getenv('POSTGRES_USER', 'rozprava'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': 5432
     }
 }
+
 
 
 # Password validation
