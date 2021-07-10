4c6461d0b7e03eb97539bdf8834bebad3eef6df8
chayan007 <sonicxxx7@gmail.com>
2021-07-11 02:50:07 +0530

    adds Payment model

---
+++ payments/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from base.models import BaseModel
+
+from profiles.models import Profile
+
+
+class Payment(BaseModel):
+    """Store all payments made for the application."""
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    amount = models.FloatField(max_length=8)
+    currency = models.CharField(max_length=5)
+    unique_reference_number = models.CharField(max_length=50)
+    order_id = models.CharField(max_length=200, null=True, blank=True)
+    raw_order = models.JSONField(null=True)
+    raw_callback = models.JSONField(null=True)
+    payment_id = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.amount}'
