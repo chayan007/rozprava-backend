0cd4f0501ade65b4a840637ecdc3c122ad39e9fc
chayan-datta <sonicxxx7@gmail.com>
2021-09-04 20:44:03 +0530

    changes query

--- debate/utilities.py
+++ debate/utilities.py
@@ -7,10 +7,10 @@ def get_debate_metrics(debate: Debate):
     """Get debate activity metrics."""
     try:
         return [
-            Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.REPORT),
-            Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.UPVOTE),
-            Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.DOWNVOTE),
-            Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.VIEW)
+            Activity.objects.filter(content_object=debate, activity_type=Activity.ActivityChoices.REPORT),
+            Activity.objects.filter(content_object=debate, activity_type=Activity.ActivityChoices.UPVOTE),
+            Activity.objects.filter(content_object=debate, activity_type=Activity.ActivityChoices.DOWNVOTE),
+            Activity.objects.filter(content_object=debate, activity_type=Activity.ActivityChoices.VIEW)
         ]
     except Exception:
         # TODO: Solve this query.
