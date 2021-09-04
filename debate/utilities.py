539e35c973adeac917fa43fd49c1760c04663374
chayan-datta <sonicxxx7@gmail.com>
2021-09-04 19:51:00 +0530

    solve error

--- debate/utilities.py
+++ debate/utilities.py
@@ -5,9 +5,13 @@ from debate.models import Debate
 
 def get_debate_metrics(debate: Debate):
     """Get debate activity metrics."""
-    return [
-        Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.REPORT),
-        Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.UPVOTE),
-        Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.DOWNVOTE),
-        Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.VIEW)
-    ]
+    try:
+        return [
+            Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.REPORT),
+            Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.UPVOTE),
+            Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.DOWNVOTE),
+            Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.VIEW)
+        ]
+    except Exception:
+        # TODO: Solve this query.
+        return [0, 0, 0, 0]
