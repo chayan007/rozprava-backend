a14a78416553cc30278dca1e62518ae7a31a6ac4
chayan-datta <sonicxxx7@gmail.com>
2021-09-04 19:49:17 +0530

    id to uuid

--- debate/utilities.py
+++ debate/utilities.py
@@ -6,8 +6,8 @@ from debate.models import Debate
 def get_debate_metrics(debate: Debate):
     """Get debate activity metrics."""
     return [
-        Activity.objects.filter(content_type=Debate, object_id=debate.id, activity_type=Activity.ActivityChoices.REPORT),
-        Activity.objects.filter(content_type=Debate, object_id=debate.id, activity_type=Activity.ActivityChoices.UPVOTE),
-        Activity.objects.filter(content_type=Debate, object_id=debate.id, activity_type=Activity.ActivityChoices.DOWNVOTE),
-        Activity.objects.filter(content_type=Debate, object_id=debate.id, activity_type=Activity.ActivityChoices.VIEW)
+        Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.REPORT),
+        Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.UPVOTE),
+        Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.DOWNVOTE),
+        Activity.objects.filter(content_type=Debate, object_id=debate.uuid, activity_type=Activity.ActivityChoices.VIEW)
     ]
