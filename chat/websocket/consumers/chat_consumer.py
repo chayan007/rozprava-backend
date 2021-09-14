9b01dd62451d2f6ec9963db02772012732b28682
chayan-datta <sonicxxx7@gmail.com>
2021-09-15 01:26:39 +0530

    chat consumer changes

--- chat/websocket/consumers/chat_consumer.py
+++ chat/websocket/consumers/chat_consumer.py
@@ -1,16 +1,24 @@
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+from chat.models import OneToOneMessage
 from profiles.models import Profile
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.room_name = None
         self.room_group_name = None
         self.sender = None
         self.receiver = None
+
+    @sync_to_async
+    def save_message(self, sender_username, receiver_username, message):
+        sender = Profile.objects.get(user__username=sender_username)
+        receiver = Profile.objects.get(user__username=receiver_username)
+        OneToOneMessage.objects.create(sender=sender, receiver=receiver, message=message)
 
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -39,6 +47,10 @@ class ChatConsumer(WebsocketConsumer):
     async def receive(self, text_data, byte_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        sender, receiver = text_data_json['room_name'].split()
+
+        # Save data to model
+        await self.save_message(sender, receiver, message)
 
         # Send message to room group
         await self.channel_layer.group_send(
--- chat/websocket/routing.py
+++ chat/websocket/routing.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from chat.websocket.consumers.chat_consumer import ChatConsumer
 
 websocket_urlpatterns = [
-    url(r'^ws/chat$', ChatConsumer),
+    url(r'^ws/chat/(?P<room_name>\w+)/$', ChatConsumer),
 ]
 
 application = ProtocolTypeRouter({
