4c7e3b033f906dbdd2215180df9d850c4edc8e21
chayan-datta <sonicxxx7@gmail.com>
2021-09-18 17:00:10 +0530

    URL issues

--- debate/urls.py
+++ debate/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
         path('activity/<debate_uuid>/<activity_type>', DebateActivityView.as_view(), name='activity'),
         path('detail/<debate_uuid>/', DebateView.as_view(), name='detail'),
         path('create/<case_uuid>/', DebateCreateView.as_view(), name='create'),
-        path('rebuttal/create/<case_uuid>/', RebuttalCreateView.as_view(), name='rebuttal-create'),
+        path('rebuttal/create/', RebuttalCreateView.as_view(), name='rebuttal-create'),
         path('rebuttal/detail/<case_uuid>/', RebuttalView.as_view(), name='rebuttal-detail'),
     ])),
 ]
