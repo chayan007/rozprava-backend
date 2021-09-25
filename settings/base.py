6a18f7375af0c79b13512a4255f667bd1b4f57db
chayan-datta <sonicxxx7@gmail.com>
2021-09-25 19:59:54 +0530

    adds pagination

--- settings/base.py
+++ settings/base.py
@@ -109,7 +109,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 50
 }
 
 REST_AUTH_SERIALIZERS = {
