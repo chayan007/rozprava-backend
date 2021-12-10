adbafc2c1760f5ed4a1be679a40a85868b8cdb63
chayan-datta <sonicxxx7@gmail.com>
2021-12-10 15:20:18 +0530

    display activity

--- case/serializers.py
+++ case/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from activity.models import Activity
+
 from case.models import Case
 from case.utilities import get_case_metrics
 
 from profiles.serializers import ProfileSerializer
+from profiles.utilities import check_if_request_authenticated
 
 from proof.serializers import ProofSerializer
 
@@ -13,6 +16,21 @@ class CaseSerializer(serializers.ModelSerializer):
     profile = serializers.SerializerMethodField()
     metrics = serializers.SerializerMethodField()
     proofs = ProofSerializer(many=True)
+    activity = serializers.SerializerMethodField()
+
+    def get_activity(self, obj):
+        request = self.context.get('request')
+        if not check_if_request_authenticated(request):
+            return {}
+
+        return Activity.objects.values_list(
+            'activity_type',
+            flat=True
+        ).filter(
+            content_type__model='case',
+            object_id=obj.uuid,
+            profile=request.user.profile
+        )
 
     def get_profile(self, obj):
         if not obj.is_anonymous:
--- debate/serializers.py
+++ debate/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
+from activity.models import Activity
+
 from debate.controllers.debate_impact_handler import DebateImpactHandler
 from debate.models import Debate
 from debate.utilities import get_debate_metrics
 
 from profiles.serializers import ProfileSerializer
+from profiles.utilities import check_if_request_authenticated
 
 from proof.serializers import ProofSerializer
 
@@ -15,6 +18,21 @@ class DebateSerializer(serializers.ModelSerializer):
     proofs = ProofSerializer(many=True)
     activities = serializers.SerializerMethodField()
     impact = serializers.SerializerMethodField()
+    activity = serializers.SerializerMethodField()
+
+    def get_activity(self, obj):
+        request = self.context.get('request')
+        if not check_if_request_authenticated(request):
+            return {}
+
+        return Activity.objects.values_list(
+            'activity_type',
+            flat=True
+        ).filter(
+            content_type__model='debate',
+            object_id=obj.uuid,
+            profile=request.user.profile
+        )
 
     def get_activities(self, obj):
         return get_debate_metrics(obj)
